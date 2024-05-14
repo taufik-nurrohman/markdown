8f44eeec3607820925269979bc2746bbddced733
Taufik Nurrohman <hi@taufik-nurrohman.com>
2024-05-14 23:26:59 +0700

    Update

--- emphasis-walker-test.php
+++ emphasis-walker-test.php
@@ -9,15 +9,16 @@ asdf *asdf*
 TEST;
 
 function emphasize($chop) {
-    $result = "";
+    $result = [];
+    $stack = 0;
+    $c_prev = "";
     while ("" !== $chop) {
-        $c_prev = substr($result, -1);
         $c_current = $chop[0];
         $c_next = $chop[1] ?? "";
         $c_next_is_p = preg_match('/[\p{P}\p{S}]/u', $c_next);
         $c_next_is_w = "" === $c_next || preg_match('/[\p{Zs}]/u', $c_next);
         $c_prev_is_p = preg_match('/[\p{P}\p{S}]/u', $c_prev);
-        $c_prev_is_w = "" === $c_prev || preg_match('/[\p{Zz}]/u', $c_prev);
+        $c_prev_is_w = "" === $c_prev || preg_match('/[\p{Zs}]/u', $c_prev);
         $left_flank = !$c_next_is_w && (!$c_next_is_p || $c_prev_is_w || $c_prev_is_p);
         $right_flank = !$c_prev_is_w && (!$c_prev_is_p || $c_next_is_w || $c_next_is_p);
         if ('_' === $c_current) {
@@ -26,52 +27,47 @@ function emphasize($chop) {
         } else if ('"' === $c_current || "'" === $c_current) {
             $can_open = $left_flank && !$right_flank;
             $can_close = $right_flank;
-        } else {
+        } else if ('*' === $c_current) {
             $can_open = $left_flank;
             $can_close = $right_flank;
+        } else {
+            $can_open = false;
+            $can_close = false;
         }
-        // // Left flank
-        // if ('*' === $c_current && (
-        //     // (1)
-        //     !preg_match('/[\p{Zs}]/u', $c_next) && (
-        //         // (2a)
-        //         !preg_match('/[\p{P}]/u', $c_next) ||
-        //         // (2b)
-        //         ("" === $c_prev || preg_match('/[\p{P}\p{S}\p{Zs}]/u', $c_prev))
-        //     )
-        // )) {
-        //     $result .= '<em>';
-        //     $chop = substr($chop, 1);
-        //     continue;
-        // }
-        // // Right flank
-        // if ('*' === $c_current && (
-        //     // (1)
-        //     !preg_match('/[\p{Zs}]/u', $c_prev) && (
-        //         // (2a)
-        //         !preg_match('/[\p{P}]/u', $c_prev) ||
-        //         // (2b)
-        //         ("" === $c_next || preg_match('/[\p{P}\p{S}\p{Zs}]/u', $c_next))
-        //     )
-        // )) {
-        //     $result .= '</em>';
-        //     $chop = substr($chop, 1);
-        //     continue;
-        // }
-        if ('*' === $c_current && $can_open) {
-            $result .= '<em>';
+        if ($c_prev === $c_current || $c_current === $c_next) {
+            $can_open = false;
+            $can_close = false;
+        }
+        if ($can_open) {
+            $result[] = ['<em>', $c_prev = $c_current];
             $chop = substr($chop, 1);
+            $stack += 1;
             continue;
         }
-        if ('*' === $c_current && $can_close) {
-            $result .= '</em>';
+        if ($can_close) {
+            $result[] = ['</em>', $c_prev = $c_current];
             $chop = substr($chop, 1);
+            $stack -= 1;
             continue;
         }
-        $result .= $c_current;
+        $result[] = $c_prev = $c_current;
         $chop = substr($chop, 1);
     }
-    return $result;
+    echo $stack . '<br>';
+    if (0 === $stack) {
+        foreach ($result as &$r) {
+            if (is_array($r)) {
+                $r = $r[0];
+            }
+        }
+        return implode("", $result);
+    }
+    foreach ($result as &$r) {
+        if (is_array($r)) {
+            $r = $r[1];
+        }
+    }
+    return implode("", $result);
 }
 
 foreach ([
@@ -84,6 +80,32 @@ foreach ([
     '(**)*',
     'asdf*)*',
     '*)*',
+    '**asdf*asdf*',
+    '*asdf*asdf**',
+    '**asdf* asdf*',
+    '*asdf *asdf**',
+] as $test) {
+    echo '<pre style="border: 1px solid;">';
+    echo htmlspecialchars(emphasize($test));
+    echo '</pre>';
+}
+
+echo '<hr>';
+
+foreach ([
+    '_(_',
+    '_(_asdf',
+    '_(__)',
+    '_(__)_',
+    '_(_asdf_)_',
+    '_(_ asdf _)_',
+    '(__)_',
+    'asdf_)_',
+    '_)_',
+    '__asdf_asdf_',
+    '_asdf_asdf__',
+    '__asdf_ asdf_',
+    '_asdf _asdf__',
 ] as $test) {
     echo '<pre style="border: 1px solid;">';
     echo htmlspecialchars(emphasize($test));
