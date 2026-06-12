b6ac9ad1f2d5de5e5b97742ac2cc57d8f67180ed
Taufik Nurrohman <hi@taufik-nurrohman.com>
2026-06-12 18:48:37 +0700

    Update

--- .dev/test/from.php
+++ .dev/test/from.php
@@ -37,6 +37,59 @@ if (!function_exists('array_is_list')) {
     }
 }
 
+function highlight_php(string $text) {
+    $r = "";
+    foreach (token_get_all($text) as $t) {
+        if (is_array($t)) {
+            $color = '00b';
+            switch ($t[0]) {
+                case T_COMMENT:
+                case T_DOC_COMMENT:
+                    $color = 'f70';
+                    break;
+                case T_CLOSE_TAG:
+                case T_OPEN_TAG:
+                case T_OPEN_TAG_WITH_ECHO:
+                    $color = '00b';
+                    break;
+                case T_INLINE_HTML:
+                    $color = '000';
+                    break;
+                case T_ABSTRACT:
+                case T_CLASS:
+                case T_ECHO:
+                case T_ELSE:
+                case T_ELSEIF:
+                case T_FINAL:
+                case T_FOR:
+                case T_FOREACH:
+                case T_FUNCTION:
+                case T_IF:
+                case T_NAMESPACE:
+                case T_NEW:
+                case T_PRINT:
+                case T_PRIVATE:
+                case T_PROTECTED:
+                case T_PUBLIC:
+                case T_RETURN:
+                case T_STATIC:
+                case T_USE:
+                case T_WHILE:
+                    $color = '070';
+                    break;
+                case T_CONSTANT_ENCAPSED_STRING:
+                case T_ENCAPSED_AND_WHITESPACE:
+                    $color = 'd00';
+                    break;
+            }
+            $r .= '<span style="color:#' . $color . ';">' . htmlspecialchars($t[1]) . '</span>';
+            continue;
+        }
+        $r .= '<span style="color:#070;">' . htmlspecialchars($t) . '</span>';
+    }
+    return $r;
+}
+
 // <https://github.com/mecha-cms/mecha/blob/v3.2.0/engine/f.php#L1606-L1671>
 function tidy_export($value, $d = "", $key_as_string = false, $is_object = null) {
     if (is_object($value)) {
@@ -292,7 +345,7 @@ foreach ($files as $file) {
     $r .= '</pre>';
     $r .= '<pre>';
     $lot = [];
-    $r .= htmlspecialchars("<?php\n\nreturn " . tidy_export(x\markdown\from\raws($raws)) . ';');
+    $r .= highlight_php("<?php\n\nreturn " . tidy_export(x\markdown\from\raws($raws)) . ';');
     $r .= '</pre>';
     $r .= '</div>';
 }
