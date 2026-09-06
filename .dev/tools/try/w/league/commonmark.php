af499713da21f3467fd38e293b6ae8a3a980c10c
Taufik Nurrohman <hi@taufik-nurrohman.com>
2026-09-06 19:56:56 +0700

    Fix Missing Fotnote Extension

--- .dev/tools/try/w/league/commonmark.php
+++ .dev/tools/try/w/league/commonmark.php
@@ -4,15 +4,17 @@ use League\CommonMark\Environment\Environment;
 use League\CommonMark\Extension\Attributes\AttributesExtension;
 use League\CommonMark\Extension\CommonMark\CommonMarkCoreExtension;
 use League\CommonMark\Extension\DescriptionList\DescriptionListExtension;
+use League\CommonMark\Extension\Footnote\FootnoteExtension;
 use League\CommonMark\Extension\Table\TableExtension;
 use League\CommonMark\MarkdownConverter;
 
 $t = hrtime(true);
 $env = new Environment;
+$env->addExtension(new AttributesExtension);
 $env->addExtension(new CommonMarkCoreExtension);
 $env->addExtension(new DescriptionListExtension);
+$env->addExtension(new FootnoteExtension);
 $env->addExtension(new TableExtension);
-$env->addExtension(new AttributesExtension);
 $r = (new MarkdownConverter($env))->convert($content)->getContent();
 $t = (hrtime(true) - $t) / 1e6;
 
